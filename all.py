e55ec2664378183d85d13fef752802163448c00a
devjolt <tom.m.warburton@gmail.com>
2022-07-08 17:05:51 +0100

    dealing with resource input questions folder

--- all.py
+++ all.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
-import platform
 
-if platform.system() == 'Windows':
-    resource_input_questions_folder = Path('./question_logic/resource_input_questions')
-else:
+try:
+    resource_input_questions_folder = Path('./question_logic/resource_input_questions/')
+    resource_dir = list(os.listdir(resource_input_questions_folder))
+
+except FileNotFoundError:
     resource_input_questions_folder = Path('mysite/question_logic/resource_input_questions/')
+    resource_dir = list(os.listdir(resource_input_questions_folder))
 
-for item in os.listdir(resource_input_questions_folder):
+for item in resource_dir:
     if item not in ['__init__.py', 'utility_logic.py', '__pycache__']:
         #print('asdf',item)
         exec(f'from .resource_input_questions import {item[:-3]}')
